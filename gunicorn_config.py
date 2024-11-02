982cc8eaa976b300e9f3f666ebc2ad7475936207
Ganesh540-crypto <ganeshprasadrock.tiwari@gmail.com>
2024-11-02 14:04:25 +0530

    ..

---
+++ gunicorn_config.py
@@ -0,0 +1,5 @@
+timeout = 120  # Increase timeout from default 30s
+workers = 4    # Number of worker processes
+threads = 2    # Threads per worker
+worker_class = 'gthread'  # Use threads
+keepalive = 65
